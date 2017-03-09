0822d92ba8599711ff43755d3aa76bf10d748c7d
Alexander Immer <alexander.immer@yahoo.com>
2017-03-09 14:54:25 +0100

    Update pypi settings

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-"""PPyGIS
+"""ppygis3 (Port from PPyGIS)
 
 Copyright (c) 2011, Bartosz Fabianowski <bartosz@fabianowski.eu>
 All rights reserved.
@@ -24,50 +24,19 @@ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
 SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
 CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."""
+from distutils.core import setup
 
-$Id: setup.py 5 2011-04-13 16:17:20Z plush $
-"""
-import distutils.core
+current_version = '0.1'
 
-version='0.2'
-
-long_description = """\
-'PPyGIS is an extension to Psycopg_, a PostgreSQL_ database adapter for the
-Python_ programming language. PPyGIS adds support for the geometry object
-types used by PostGIS_.
-
-PPyGIS has been tested with Python 2.7. It may work with earlier versions as
-well. A Python 3 version does not currently exist.
-
-.. _Psycopg: http://initd.org/psycopg/
-.. _PostgreSQL: http://www.postgresql.org/
-.. _Python: http://www.python.org/
-.. _PostGIS: http://postgis.refractions.net/'
-"""
-
-distutils.core.setup(
-    name='PPyGIS',
-    version=version,
-    description='PostGIS adapter for Psycopg',
-    long_description=long_description,
-    author='Bartosz Fabianowski',
-    author_email='bartosz@fabianowski.eu',
-    url='http://www.fabianowski.eu/projects/ppygis/',
-    download_url=
-      'http://www.fabianowski.eu/projects/ppygis/releases/' + \
-      'PPyGIS-' + version + '.tar.gz',
-    packages=[''],
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Environment :: Other Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Topic :: Database :: Front-Ends',
-        'Topic :: Scientific/Engineering :: GIS',
-        'Topic :: Software Development :: Libraries :: Python Modules'],
-    keywords=['psycopg', 'postgis', 'geometry', 'EWKB'],
-    package_dir={'': 'src'})
+setup(
+    name='ppygis3',
+    version=current_version,
+    description='PostGIS geometry objects synchronized in Python',
+    author='Alexander Immer', # original author: Bartosz Fabianowski
+    author_email='alexndr.immer@gmail.com', # original author: bartosz@fabianowski.eu
+    url='https://github.com/AlexImmer/ppygis3',
+    download_url='https://github.com/AlexImmer/ppygis3/archive/' + current_version + '.tar.gz',
+    keywords=['postgreSQL', 'postgis', 'EWKB', 'geometry', 'psycopg'],
+    classifiers=[]
+)
