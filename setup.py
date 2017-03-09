28aca5f5d4d4b5722033fe877c9aae2a74a5f6da
Alexander Immer <alexander.immer@yahoo.com>
2017-03-09 17:28:38 +0100

    Change to version 0.3

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."""
 from distutils.core import setup
 
-current_version = '0.2.1'
+current_version = '0.3'
 
 setup(
     name='ppygis3',
